cc89d09f0103021f7a04c4e4f4def260615c1396
lpha <kyo@unicorn.inc>
2021-11-25 12:01:35 +0900

    fix keepcdata

--- xml/xml.go
+++ xml/xml.go
@@ -23,7 +23,7 @@ var DefaultMinifier = &Minifier{}
 // Minifier is an XML minifier.
 type Minifier struct {
 	KeepWhitespace bool
-        KeepCDATA      bool
+	KeepCDATA      bool
 }
 
 // Minify minifies XML data, it reads from r and writes to w.
@@ -45,15 +45,14 @@ func (o *Minifier) Minify(m *minify.M, w io.Writer, r io.Reader, _ map[string]st
 	for {
 		t := *tb.Shift()
 		if t.TokenType == xml.CDATAToken {
-			if o.KeepCDATA {
-				continue
-                        }
 			if len(t.Text) == 0 {
 				continue
 			}
-			if text, useText := xml.EscapeCDATAVal(&attrByteBuffer, t.Text); useText {
-				t.TokenType = xml.TextToken
-				t.Data = text
+			if !o.KeepCDATA {
+				if text, useText := xml.EscapeCDATAVal(&attrByteBuffer, t.Text); useText {
+					t.TokenType = xml.TextToken
+					t.Data = text
+				}
 			}
 		}
 		switch t.TokenType {
